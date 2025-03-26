72863ec3e2eaaaa61789360ee38c272829c0141a
Alitey <alyalu004@gmail.com>
2025-03-26 14:03:58 +0300

    fix settigs names

--- auth_app/core/settings.py
+++ auth_app/core/settings.py
@@ -2,16 +2,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 import os 
 
 class AuthSettings(BaseSettings):
-    DB_NAME: str
-    DB_USER: str
-    DB_HOST: str
-    DB_PORT: str
-    DB_PASSWORD: str
+    PGHOST: str
+    PGDATABASE: str
+    PGUSER: str
+    PGPASSWORD: str
+    PGPORT: str
 
     model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../.env"), extra="ignore")
 
     def db_connection_url(self):
-        return f"postresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        return f"postresql+asyncpg://{self.PGUSER}:{self.PGPASSWORD}@{self.PGHOST}:{self.PGPORT}/{self.PGDATABASE}"
     
 auth_settings = AuthSettings()
 
